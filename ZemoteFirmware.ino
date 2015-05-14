0a6b8af56291e219b5c2d8e9f794a0b52fd27984
Cameron Lai <cameronjml2@gmail.com>
2015-05-14 23:27:22 +0800

    Update license info in each header

--- ZemoteFirmware.ino
+++ ZemoteFirmware.ino
@@ -1,18 +1,19 @@
-/*
- * ZemoteFirmware
- * (C) Copyright 2014 Cameron Lai
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Lesser General Public License
- * (LGPL) version 3.0 which accompanies this distribution, and is available at
- * https://www.gnu.org/licenses/lgpl-3.0.txt
- *
- * ZemoteFirmware is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- */
+/* 
+* This file is part of ZemoteFirmware.
+* 
+* ZemoteFirmware is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* ZemoteFirmware is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with ZemoteFirmware.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #define VERSION_NUM 0.1
 
